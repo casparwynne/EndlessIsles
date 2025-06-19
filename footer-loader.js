5db1f4ab34d0b4ffd6bf02f36afce66b76a7c898
casparwynne <51761761+casparwynne@users.noreply.github.com>
2025-06-19 23:30:46 +0100

    Delete footer-loader.js

--- footer-loader.js
+++
@@ -1,8 +0,0 @@
-fetch('_footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer-container').innerHTML = data;
-    document.getElementById('year').textContent = new Date().getFullYear();
-
-  })
-  .catch(error => console.error('Footer load error:', error));
